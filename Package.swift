67053f4c315b7890b5b728c848aab985c533934c
Daniel Saidi <daniel.saidi@gmail.com>
2024-10-04 18:22:00 +0200

    Update to Swift 6

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 
 import PackageDescription
 
@@ -19,10 +19,7 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "EnvironmentKit",
-            swiftSettings: [
-                .enableExperimentalFeature("StrictConcurrency")
-            ]
+            name: "EnvironmentKit"
         ),
         .testTarget(
             name: "EnvironmentKitTests",
